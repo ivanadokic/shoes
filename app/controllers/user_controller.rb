0f4a4be9b85983fb09e2e8436b6af5aca0ab6344
Ivana <ivana_pile@yahoo.com>
2020-01-15 20:51:47 -0500

    user_controller edit patch and delete

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -52,5 +52,27 @@ class UserController < ApplicationController
      else
         redirect "/login"
     end
- end
+
     #did they give a valid password?
+
+    # GET: /users/5
+  get "/users/:id" do
+    erb :"/users/show.html"
+  end
+
+  # GET: /users/5/edit
+  get "/users/:id/edit" do
+    erb :"/users/edit.html"
+  end
+
+  # PATCH: /users/5
+  patch "/users/:id" do
+    redirect "/users/:id"
+  end
+
+  # DELETE: /users/5/delete
+  delete "/users/:id/delete" do
+    redirect "/users"
+  end
+  
+end
